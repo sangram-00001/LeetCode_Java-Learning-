ce482127383fca4056b7a59a80d29206d4f5f2ce
Sangram Rath <sangramrath0001@gmail.com>
2026-03-14 23:48:24 +0530

    Add comments to Fibonacci function variables
    Added comments to clarify variable purposes.

--- LC509_Fibonacci_Number.java
+++ LC509_Fibonacci_Number.java
@@ -2,10 +2,10 @@ class Solution{
     public int fibonacci(int n){
         if(n==0) return 0;
         if(n==1) return 1;
-        int ft=0;
-        int st=1;
+        int ft=0;//firstTerm
+        int st=1;//SecondTerm
         for(int i=1;i<n;i++){
-            int tt=ft+st;
+            int tt=ft+st;//ThirdTerm
             ft=st;
             st=tt;
         }
