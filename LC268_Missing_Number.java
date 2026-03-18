d62379231fc328a37ad6d4924ec90062fd67b918
Sangram Rath <sangramrath0001@gmail.com>
2026-03-19 00:02:15 +0530

    missingNumber

---
+++ LC268_Missing_Number.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+class Solution {
+    public int missingNumber(int[] nums) {
+        int range=nums.length;
+        int actualSum=(range*(range+1))/2;
+        int currSum=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            currSum=currSum+nums[i];
+        }
+        int ans=actualSum-currSum;
+        return ans;
+    }
+}
